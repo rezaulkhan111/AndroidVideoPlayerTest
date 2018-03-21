3c6afc3573889643c8cdc4f6a56e1f0d74025da3
rezaulkhan111 <rezaulkhan111@hotmail.com>
2018-03-21 18:07:04 +0600

    Initial commit

---
+++ app/src/main/java/com/example/entern/androidvideoplayertest/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.entern.androidvideoplayertest;
+
+import android.net.Uri;
+import android.support.v4.media.session.MediaControllerCompat;
+import android.support.v4.media.session.MediaSessionCompat;
+import android.support.v4.media.session.PlaybackStateCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        VideoView videoView =(VideoView)findViewById(R.id.vv_video_player);
+        MediaController mediaController= new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        Uri uri= Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.video);
+        videoView.setMediaController(mediaController);
+        videoView.setVideoURI(uri);
+        videoView.requestFocus();
+
+        videoView.start();
+    }
+}
